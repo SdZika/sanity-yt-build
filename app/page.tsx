1ae032403083fb1eb63cf84f7b4c96ff204cd1db
SdZika <markozivkovic1982@gmail.com>
2025-01-29 14:42:25 +0100

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Header } from "@/components/Header";
+
+export default function Home() {
+  return (
+    <>
+      <head>
+        <title>Medium Blog</title>
+      </head>
+      <Header />
+    </>
+  );
+}
